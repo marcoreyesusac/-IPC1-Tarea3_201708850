7bead4e562dc5c26e0b121fa4bd947e0618346d7
admin <admin@192.168.1.9>
2018-02-20 19:57:50 -0600

    Estoy haciendo mi tarea

---
+++ src/taream/TareaM.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taream;
+
+/**
+ *
+ * @author admin
+ */
+
+
+
+import java.util.Scanner;
+
+public class TareaM {
+
+
+}
+
+    
+
